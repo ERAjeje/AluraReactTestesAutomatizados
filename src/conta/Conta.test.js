024374886363635b9492689951b5c56632d4c9d0
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-04 18:48:46 -0300

    spi e mock de função

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Conta from "./Conta";
 
@@ -9,5 +9,31 @@ describe('Componente de conta', () => {
         const saldo = screen.getByTestId('saldo-conta');
 
         expect(saldo.textContent).toBe('R$ 1000');
+    })
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+        const realizarTransacao = jest.fn();
+        render(<Conta saldo={1000} realizarTransacao={realizarTransacao} />);
+
+        const botao = screen.getByText('Realizar operação');
+
+        const valor = screen.getByTestId('valor');
+        const transacao = screen.getByLabelText('Saque');
+        
+        fireEvent.click(transacao, { target: { value: 'saque' }});
+        fireEvent.change(valor, { target: { value: 10 }});
+        
+        if(valor.textContent !== 0 && transacao.target !== '')
+            fireEvent.click(botao)
+
+        expect(realizarTransacao).toHaveBeenCalled();
+    })
+    it('A função realizar transação não é chamada pois o botão não foi habilitado', () => {
+        const realizarTransacao = jest.fn();
+        render(<Conta saldo={1000} realizarTransacao={realizarTransacao} />);
+
+        const botao = screen.getByText('Realizar operação');
+
+        fireEvent.click(botao);
+        expect(realizarTransacao).not.toHaveBeenCalled();
     })
 })
